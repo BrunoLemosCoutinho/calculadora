628490fb73b48f38bcde8bbc3aa8cefe2e4f46de
Bruno Lemos Coutinho <brunolemos.ssa@gmail.com>
2019-05-14 14:36:30 -0300

    Adicionado arquivo main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter
+import application
+
+
+class TkinterWindow(tkinter.Tk):
+	def __init__(self):
+		super().__init__()
+
+
+def main():
+	window = TkinterWindow()
+	app = application.Calculator(window)
+	app.pack(fill='both', expand=1, padx=10, pady=10)
+	app.mainloop()
+
+
+if __name__ == '__main__':
+	main()
