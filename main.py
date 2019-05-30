3f6ccb4b179d557dd2ad1247c855bd78842199cf
Bruno Lemos Coutinho <brunolemos.ssa@gmail.com>
2019-05-29 23:09:15 -0300

    Organizacao do codigo de acordo com PEP8

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import application
 
 
 class TkinterWindow(tkinter.Tk):
-	def __init__(self):
-		super().__init__()
-		self.geometry('640x480')
+    def __init__(self):
+        super().__init__()
+        self.geometry('640x480')
 
 
 def main():
-	window = TkinterWindow()
-	app = application.Calculator(window)
-	app.pack(fill='both', expand=1, padx=10, pady=10)
-	app.mainloop()
+    window = TkinterWindow()
+    app = application.Calculator(window)
+    app.pack(fill='both', expand=1, padx=10, pady=10)
+    app.mainloop()
 
 
 if __name__ == '__main__':
-	main()
+    main()
